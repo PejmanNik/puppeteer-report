0088bb747e9fa1c9c11166b9d217afe1de45a6c7
Emil Ajdyna <emil@ajdyna.com>
2021-05-21 16:03:55 +0200

    Ensure that pdfs are generated

--- test/index.js
+++ test/index.js
@@ -1,7 +1,25 @@
-const test = require('ava')
-const report = require('../out')
+const test = require("ava")
+const puppeteer = require("puppeteer")
+const { join } = require("path")
+const { tmpdir } = require("os")
+const { promises: { readFile } } = require("fs")
+const report = require("../out")
 
-test('report', assert => {
-  assert.truthy(report.pdf)
-  assert.truthy(report.pdfPage)
+test("it generates a pdf", async assert => {
+  const browser = await puppeteer.launch()
+  const input = join(__dirname, 'fixtures', 'basic.html')
+  const output = join(tmpdir(), 'basic.pdf')
+  await report.pdf(browser, input, {
+      path: output,
+      format: "a4",
+      margin: {
+          bottom: '10mm',
+          left: '10mm',
+          right: '10mm',
+          top: '10mm'
+      }
+  })
+  const file = await readFile(output)
+  assert.truthy(file)
+  await browser.close()
 })
